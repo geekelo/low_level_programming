e9c7055da4af3d01c60cb7f65e3be4248ea0df48
geekelo <efuelight12@gmail.com>
2023-02-16 12:40:19 -0800

    std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- *
- * Return : Always 1 (Error)
+ * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n", 59);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n\n", 59);
 return (1);
 }
-
