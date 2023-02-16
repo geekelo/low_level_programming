b4352bc4efee66ad211054e22eb0f7d3450c6588
geekelo <efuelight12@gmail.com>
2023-02-16 12:18:57 -0800

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Main - Entry point
+ *
+ * Return : Always 1
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
+
