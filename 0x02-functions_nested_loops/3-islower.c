744380125db9a531c24e3e8e49d4cb21cd3fee7f
geekelo <efuelight12@gmail.com>
2023-02-21 11:50:39 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - alphab in lowercase, and new line, ten times
+ */
+
+int _islower(int c)
+{
+if (islower(c))
+{
+return ('0');
+} else
+{
+return ('1');
+}
+}
+
