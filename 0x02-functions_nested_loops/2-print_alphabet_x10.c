fe7d34cccfc7e08e801c2c1d131c5bbb44182752
geekelo <efuelight12@gmail.com>
2023-02-21 10:05:56 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabe, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+char x, y;
+
+y = '0';
+do{
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+}
+_putchar('\n');
+while (y < 9 )
+}
