95478e1f995d9303d63fe1f9fc3261c348c57195
geekelo <efuelight12@gmail.com>
2023-02-21 14:48:00 -0800

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,7 @@
 void print_alphabet(void)
 {
 char x;
+
 for (x = 'a'; x <= 'z'; x++)
 {
 _putchar(x);
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,11 +12,11 @@ int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return (0);
+return (1);
 }
 else
 {
-return (1);
+return (0);
 }
 }
 
