2a46b1bb73dd7403603cc6c90a7ac1d0aef5ed7c
geekelo <efuelight12@gmail.com>
2023-02-21 11:12:22 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-char x;
-int y;
+char x, y;
 
-y = '0';
-while (y < 10)
+for (y = '0'; y < '10'; y++)
 {
 for (x = 'a'; x <= 'z'; x++)
 {
 _putchar(x);
 }
 _putchar('\n');
-y++;
 }
 }
