916daf192a992c2a969201a6110f0d6ccf061d69
geekelo <efuelight12@gmail.com>
2023-02-21 09:32:56 -0800

    prototyping

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,8 @@
  * print_alphabet - prints the alphabe, in lowercase, followed by a new line
  */
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
 char x;
 for (x = 'a'; x <= 'z'; x++)
 {
