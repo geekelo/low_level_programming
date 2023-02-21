92a3424eefa822c0db411b0b30ad51585e84504a
geekelo <efuelight12@gmail.com>
2023-02-21 15:59:09 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - prints absolute value
+ * @n: The character to print
+ *
+ * Return: returns zero false and one and true
+ *
+ */
+
+int _abs(int)
+{
+abs(int)
+}
