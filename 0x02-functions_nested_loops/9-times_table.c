97dd0583111cf327188d0c82f8a40dfd0c560053
geekelo <efuelight12@gmail.com>
2023-02-22 12:16:56 +0100

    Task 9 test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - prints all minutes in a day from 00:00
+ * times_table - prints 9 times table
  *
  * Return:0 if succeesful.
  */
@@ -8,20 +8,16 @@
 
  void times_table(void)
 {
-int i;
+int i, y, j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i * '0');
-		_putchar(i * '1');
-		_putchar(i * '2');
-		_putchar(i * '3');
-		_putchar(i * '4');
-		_putchar(i * '5');
-		_putchar(i * '6');
-		_putchar(i * '7');
-		_putchar(i * '8');
-		_putchar(i * '9');
-		_putchar('\n');
+		for (j = '0'; j <= '9'; j++)
+		{
+		y = i * j;
+		_putchar(y);
+		_putchar(',');
+		}
+	_putchar('\n');
 	}
 }
