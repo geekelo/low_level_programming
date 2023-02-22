4d36c3e3c4a78451a11256fe94693c609f80a785
geekelo <efuelight12@gmail.com>
2023-02-22 14:58:28 +0100

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * print_to_98 - returns num fro n - 98
- *
- * @n: parameter to start form
+* print_to_98 - returns num fro n - 98
+*
+* @n: parameter to start form
 */
 
 void print_to_98(int n)
 {
 int i;
 
-    for (i = n; i < 99; i++)
-    {
-    if (n < 0)
-    {
-        _putchar(45);
-        _putchar((n % 10) + 48);
-    }
-    else if (n > 9)
-    {
-        _putchar((n / 10) + 48);
-        _putchar((n % 10) + 48);    
-    }
-    else
-    {
-    _putchar(n + 48);
-    }
+for (i = n; i < 99; i++)
+{
+if (n < 0)
+{
+_putchar(45);
+_putchar((n % 10) + 48);
+}
+else if (n > 9)
+{
+_putchar((n / 10) + 48);
+_putchar((n % 10) + 48);    
+}
+else
+{
+_putchar(n + 48);
+}
 
 _putchar(44);
 _putchar(32);
