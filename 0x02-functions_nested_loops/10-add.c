4cca09e965db0c5e7abc635d7cc12efbfe9dca79
geekelo <efuelight12@gmail.com>
2023-02-22 13:28:29 +0100

    Task 9 test

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * add - adds sum of numbers
  * 
+ * n: first parameter
+ * c: second parameter
+ *  
+ * Return: the sum
 */
 
+int add(int n, int c)
+{
+ return (n + c);
+}
