2ce7a8a78e58b2d6561132c04834976db185fa0d
geekelo <efuelight12@gmail.com>
2023-02-22 11:40:19 +0100

    TAST8 TEST

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,5 +8,27 @@
 
 void jack_bauer(void)
 {
-_putchar('9');
+int i, j, k, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
