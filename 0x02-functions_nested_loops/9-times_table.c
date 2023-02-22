494bdbfb07001ccc0fe5b983a6aebb45e7355d23
geekelo <efuelight12@gmail.com>
2023-02-22 12:04:19 +0100

    TAST9 TEST

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,18 +10,18 @@
 {
 int i;
 
-	for (i = '0'; i <= '2'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i * 0);
-		_putchar(i * 1);
-		_putchar(i * 2);
-		_putchar(i * 3);
-		_putchar(i * 4);
-		_putchar(i * 5);
-		_putchar(i * 6);
-		_putchar(i * 7);
-		_putchar(i * 8);
-		_putchar(i * 9);
+		_putchar(i * '0');
+		_putchar(i * '1');
+		_putchar(i * '2');
+		_putchar(i * '3');
+		_putchar(i * '4');
+		_putchar(i * '5');
+		_putchar(i * '6');
+		_putchar(i * '7');
+		_putchar(i * '8');
+		_putchar(i * '9');
 		_putchar('\n');
 	}
 }
