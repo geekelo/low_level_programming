a0ab2abac83ed49846b31c4be1cc604518f4b730
geekelo <efuelight12@gmail.com>
2023-02-21 16:16:29 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - checks for letters only*
+ * @n: The character to print
+ *
+ * Return: returns zero false and one and true
+ *
+ */
+
+int print_last_digit(int n)
+{
+int lastdigit;
+
+lastdigit = n % '10';
+return (lastdigit);
+}
+
