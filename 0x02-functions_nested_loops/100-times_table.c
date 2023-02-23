b293812841d76817e47410a92653a2ebe5be7354
geekelo <efuelight12@gmail.com>
2023-02-23 02:36:32 +0100

    tASK 12ja

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,16 +16,34 @@ _putchar('0');
 _putchar('\n');
 }
 
-else if (n == 2)
+else if (n > 0 && n <= 15)
 {
-for (i = 0; i <= 2; i++)
+for (i = 0; i <= n; i++)
 {
 
-for (j = 0; j <= 2; j++)
+for (j = 0; j <= n; j++)
 {
 k = i * j;
+
+if (k > 9)
+{
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+if (j == n)
+{
+break;
+}
+_putchar(44);
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+
+else
+{
 _putchar(k + '0');
-if (j == 2){
+if (j == n)
+{
 break;
 }
 _putchar(44);
