f2f7a0a48379eac67a230bab07a91d9791492750
geekelo <efuelight12@gmail.com>
2023-02-23 05:04:23 +0100

    tASK 12jabh

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,5 +8,36 @@
 
 int main(void)
 {
+
+
+int x;
+long int y, z, sum;
+
+y = 2;
+z = 1;
+sum = 0;
+
+for (x = 1; x <= 50; x++)
+{
+sum = y + z;
+if (x == 1 || x == 2)
+{
+printf("%d, ", x);
+}
+else
+{
+if (x == 50)
+{
+printf("%ld\n", sum);
+}
+else
+{
+printf("%ld, ", sum);
+z = y;
+y = sum;
+}
+}
+}
+
 return (0);
 }
