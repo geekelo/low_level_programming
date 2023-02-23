fea2c561fd1bb5cdd80984adfdf73bf71683a29f
geekelo <efuelight12@gmail.com>
2023-02-23 01:45:02 +0100

    tASK 12e

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - prints the sum of mult 3 or 5
- * 
- * Return: Always 0
+* main - prints the sum of mult 3 or 5
+*
+* Return: Always 0
 */
 
 int main(void)
 {
-    int i, j, k, l;
+int i, j, k, l;
 
-    j = 0;
+j = 0;
 
-    for (i = 0; i < 1024; i++)
-    {
-        k = i % 3;
-        l = i % 5;
-        if (k == 0 || l == 0)
-            {
-                j = j + 1;
-            }
-    }
-    printf("%d\n", j);
+for (i = 0; i < 1024; i++)
+{
+k = i % 3;
+l = i % 5;
+if (k == 0 || l == 0)
+{
+j = j + i;
+}
+}
+printf("%d\n", j);
 
-    return (0);
+return (0);
 }
