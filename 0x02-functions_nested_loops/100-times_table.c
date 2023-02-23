9fa613fd3366c15e67bb0dbd6149200386bc72a1
geekelo <efuelight12@gmail.com>
2023-02-23 02:12:07 +0100

    tASK 12ja

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,35 +10,17 @@ void print_times_table(int n)
 {
 int i, j, k;
 
+if (n == 0)
+{
+_putchar(n);
+}
 
-if (n > 0 && n <= 15)
+else if (n > 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
-_putchar(i + 48);
-for (j = 0; i <= n; j++)
-{
-_putchar(44);
-_putchar(32);
-k = i * j;
+_putchar('0');
 
-if (k <= 9)
-{
-_putchar(32);
-_putchar(k + '0');
-}
-else
-{
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
 }
 }
-_putchar('\n');
-}
-}
-else if (n == 0)
-{
-_putchar(48);
-}
-
 }
