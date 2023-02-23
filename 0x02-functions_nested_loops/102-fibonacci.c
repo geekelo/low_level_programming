cf21a22e427c96c24f6a64dad544df3468397094
geekelo <efuelight12@gmail.com>
2023-02-23 04:06:55 +0100

    tASK 12ja

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,3 +6,22 @@
 * Return: Always 0
 */
 
+int main(void)
+{
+int x, y;
+
+y = 0;
+
+for (x = 1; x <= 50; x++)
+{
+y = y + x;
+if (x == 1 || x == 2)
+{
+printf("%d, ", x);
+}
+else
+{
+printf("%d, ", y);
+}
+}
+}
