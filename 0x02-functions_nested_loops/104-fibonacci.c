b3aecf025d527f6a3562a6482420c0e1dd061a50
geekelo <efuelight12@gmail.com>
2023-02-23 05:22:54 +0100

    tASK 12jabo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - prints the sum of mult 3 or 5
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int x;
+long int y, z, sum;
+
+y = 2;
+z = 1;
+sum = 0;
+
+for (x = 1; x <= 98; x++)
+{
+sum = y + z;
+if (x == 1 || x == 2)
+{
+printf("%d, ", x);
+}
+else
+{
+if (x == 98)
+{
+printf("%ld\n", sum);
+}
+else
+{
+printf("%ld, ", sum);
+z = y;
+y = sum;
+}
+}
+}
+return (0);
+}
