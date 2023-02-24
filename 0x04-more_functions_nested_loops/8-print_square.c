f88c3b221489c6c09a2b29f5a8cf5b0951d66eba
geekelo <efuelight12@gmail.com>
2023-02-24 19:31:08 +0100

    is digit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ void print_square(int size)
 {
 int i, j;
 
-if ( size > 0)
+if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
@@ -17,10 +17,7 @@ for (j = 0; j < size; j++)
 {
 _putchar(35);
 }
-if (i < (size - 1))
-{
 _putchar('\n');
-}
 }
 }
 else
