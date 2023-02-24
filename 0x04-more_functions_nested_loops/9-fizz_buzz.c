62f7a31a9dba1311fc575acff1dd56e52300cd3a
geekelo <efuelight12@gmail.com>
2023-02-24 20:45:32 +0100

    is digit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0  &&  i % 5 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf("FizzBuzz");
 }
@@ -30,6 +30,7 @@ printf("%d", i);
 if (i < 100)
 {
 printf(" ");
-}
-}
+}}
+printf("\n");
+return (0);
 }
