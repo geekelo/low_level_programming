b3e2e66468b3563b0ce4e78c5badd1be5debeb00
geekelo <efuelight12@gmail.com>
2023-02-24 16:23:29 +0100

    is digit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,10 @@
 
 void more_numbers(void)
 {
-int i;
+int i, j;
+
+for (j = 0; j < 10; j++)
+{
 for (i = 0; i < 15; i++)
 {
 
@@ -18,4 +21,5 @@ _putchar((i / 10) + 48);
 _putchar((i % 10) + 48);
 }
 _putchar('\n');
+}
 }
