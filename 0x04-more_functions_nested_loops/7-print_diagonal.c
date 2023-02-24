ffb180687c7f99115958c0c9e263bbe3238fe180
geekelo <efuelight12@gmail.com>
2023-02-24 19:04:26 +0100

    is digit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,20 +12,16 @@ int i, j;
 
 if (n > 0)
 {
-for (i=1; i <= n; i++)
+for (i = 1; i <= n; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 1; j < i; j++)
 {
 _putchar(32);
 }
 
 _putchar(92);
-
-if (i < n)
-{
 _putchar('\n');
 
-}
 }
 }
 else
