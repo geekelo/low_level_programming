a0ad2ea95ac5eebcac8815ee2c765687863068c0
geekelo <efuelight12@gmail.com>
2023-02-24 12:25:48 +0100

    is digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns result
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
