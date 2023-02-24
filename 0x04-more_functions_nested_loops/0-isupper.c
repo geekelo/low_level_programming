33add910c07c2dd0db5b243d62b2fe93fa670e4d
geekelo <efuelight12@gmail.com>
2023-02-24 10:09:51 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * 
+ * Return: 1 if true 0 if false
+*/
+
+
+int _isupper(char c)
+{
+    if (isupper(c))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
