3327115f32a26f52e46c2ce490d8c47f4d42e602
geekelo <efuelight12@gmail.com>
2023-02-24 20:18:53 +0100

    is digit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-* main - prints Buzz each numbers of 3 and 5.
-* Return: Always 0.
+* main - prints fizzbuzz
+* Return: Always 0
 */
+
 int main(void)
 {
-int n;
+int i;
 
-n = 1;
-printf("%d", n);
-for (n = 2; n <= 100; n++)
+printf("1");
+for (i = 2; i <= 100; i++)
 {
-if ((n % 3 == 0) && (n % 5 == 0))
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
 }
-else if (n % 3 == 0)
+else if (i % 3 == 0)
 {
 printf(" Fizz");
 }
-else if (n % 5 == 0)
+else if (i % 5 == 0)
 {
 printf(" Buzz");
 }
 else
 {
-printf(" %d", n);
+printf(" %d", i)
 }
 }
 printf("\n");
