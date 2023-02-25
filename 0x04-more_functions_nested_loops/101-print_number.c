1303d487104ee1beb08d9272238a0102553c82f6
geekelo <efuelight12@gmail.com>
2023-02-25 04:07:22 +0100

    is digit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - pints an integer
+ * @n: parameter
+*/
+
+void print_number(int n)
+{
+int i, j, k;
+
+j = sizeof(n);
+
+for (i = j; i > 1; i--)
+{
+k = n / pow(10, i);
+_putchar(k + "0");
+}
+_putchar((n % 10) + "10");
+}
