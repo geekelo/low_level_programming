cfc4d42e2035114100c44f2d48741553af8de26e
Geekelo <efuelight12@gmail.com>
2023-02-27 19:43:52 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - prints the length of the string
+* @s: parameter
+*/
+
+int _strlen(char *s)
+{
+int v;
+
+v = strlen(s);
+return (v);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 int main(void);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _strlen(char *s);
 
 #endif
