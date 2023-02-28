7e819b79c824b7d0a6dc1f4573526b1af7436a94
Geekelo <efuelight12@gmail.com>
2023-02-28 17:14:34 +0000

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-*
-*
+* print_array - prints array members
+* @n: param1
+* @a: param2
 */
 
 void print_array(int *a, int n)
 {
 int i, l;
 
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 l = *(a + i);
@@ -20,5 +23,10 @@ else
 {
 printf("%d\n", l);
 }
+}
+}
+else
+{
+printf(" \n");
 }
 }
