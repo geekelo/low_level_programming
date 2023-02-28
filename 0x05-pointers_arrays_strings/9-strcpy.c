77a74d1061cf58b56b518a2863dcf0c3c672ed56
Geekelo <efuelight12@gmail.com>
2023-02-28 17:58:09 +0000

    Tak 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* _strcpy - copies text
+* @src: param
+* @dest: param
 */
 
 char *_strcpy(char *dest, char *src)
 {
-dest = src;
-return dest;
+int i;
+
+i = 0;
+while (src[i] != '\n')
+{
+*(dest + i) = *(src + i);
+i++;
 
 }
-
+*(dest + (i + 1)) = *(src + (i + 1));
+  
+return (dest);
+}
