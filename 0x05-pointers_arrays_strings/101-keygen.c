95426eb2adfbf224d44c48ccec510010f691b163
Geekelo <efuelight12@gmail.com>
2023-03-01 04:26:51 +0000

    yyy

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,50 +1,35 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
-* main - returns random password
-* Return: Always 0
-*/
-
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-int x, y, w;
-char z[14];
-char alpa[] = "abcdefghijklmnopqrst";
-char upper[] = "ABCDEFGHIJKLMNOPQRST";
-char sym[] = "!@#$%";
-char pac[] = " ";
+ int sum = 2772;
+ int n;
 
-srand((unsigned int)(time(NULL)));
-x = rand() % 3;
-
-for (w = 0; w < 5; w++)
-{
-for (y = 0; y < 7; y++)
-{
-if (x == 1)
-{
-z[y] = upper[rand() % 20];
-}
-else if (x == 2)
-{
-z[y] = alpa[rand() % 20];
-}
-else if (x == 3)
-{
-z[y] = pac[rand() % 1];
-}
-else
-{
-z[y] = sym[rand() % 5];
-}
-
-printf("%c", z[y]);
-x = rand() % 3;
-}
-}
-
-return (0);
+ srand(time(0));
+ while (sum)
+ {
+  n = rand() % 100;
+  if (n <= 94)
+   n += 32;
+  else
+   continue;
+  if (sum - n == 0)
+  {
+   sum -= n;
+   printf("%c", n);
+  }
+  else if (sum - n - 32 > 0)
+  {
+   sum -= n;
+   printf("%c", n);
+  }
+ }
+ return (0);
 }
