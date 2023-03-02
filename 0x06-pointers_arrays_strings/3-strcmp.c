ad816e7aab950287538d0697860a6c50cef09f28
Geekelo <efuelight12@gmail.com>
2023-03-02 16:31:16 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - compare strings
+* @s1: param
+* @s2: param
+*/
+int _strcmp(char *s1, char *s2)
+{
+int k;
+
+k = strcmp(s1, s2);
+return (k);
+}
