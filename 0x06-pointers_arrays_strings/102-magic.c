22e819e12af3f5c6c04ad9a670b8dfa62a2d5773
Geekelo <efuelight12@gmail.com>
2023-03-03 12:11:55 +0000

    test

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,10 +14,10 @@ int n;
 int a[5];
 int *p;
 
-  a[2] = 1024;
-  p = &n;
-  ;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+a[2] = 1024;
+p = &n;
+;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
