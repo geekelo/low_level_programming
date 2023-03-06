dab1fbfee8c58fb164a43402b9a4c4752e5d73a6
Geekelo <efuelight12@gmail.com>
2023-03-06 16:40:27 +0000

    Task 4

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strstr - first occurrence of a substring
+* @haystack: param
+* @needle: param
+* Return: first occurence
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int len = 0;
+while (needle[len] != '\0')
+{
+int gen = 0;
+
+while (haystack[gen] != '\0')
+{
+if (needle[len] == haystack[gen])
+{
+return ((haystack + gen));
+}
+gen++;
+}
+
+len++;
+}
+return ('\0');
+}
