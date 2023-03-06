33d7bdf8a8b0c434cb14d5ebf8ac4df381a5bd09
Geekelo <efuelight12@gmail.com>
2023-03-06 23:37:24 +0000

    Task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif
