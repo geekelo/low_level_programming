320595c39a1e0f2512a97f26bc6c891c458f5ada
Geekelo <efuelight12@gmail.com>
2023-03-06 15:29:38 +0000

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strpbrk - finds the first occurence of a character
+* @s: array param
+* @c: param character
+* Return: Amemory address
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int len = 0;
+
+while (s[len] != 0)
+{
+int gen = 0;
+
+while (accept[gen] != 0)
+{
+if (s[len] == accept[gen])
+{
+break;
+}
+gen++;
+}
+if (s[len] == accept[gen])
+{
+break;
+}
+len++;
+}
+while (s[len] >= '\0')
+{
+return ((s + len));
+len++;
+}
+return ('\0');
+}
