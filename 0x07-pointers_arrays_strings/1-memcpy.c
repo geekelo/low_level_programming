1918d6518170fb4f10ff0d76d01026ed9cedf4b3
Geekelo <efuelight12@gmail.com>
2023-03-06 12:49:43 +0000

    Task 2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* _memcpy - fills memory with a constant byte
+* @s: param array  or meory to be filled
+* @b: Character to fill with
+* @n: size to fill
+* Return: an array
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
