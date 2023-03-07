d2c895bc158f7a92f1ad087537401221cef8cf2f
Geekelo <efuelight12@gmail.com>
2023-03-07 21:25:45 +0000

    Task 9

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* prime - does further check
+* @v: param
+* @n: param
+*/
+
+
+
+int prime(int v, int n)
+{
+if ((n % v) == 0)
+{
+return (0);
+}
+
+if (v > (n / 2))
+{
+return (1);
+}
+v++;
+return (prime(v, n));
+}
+
+/**
+* is_prime_number - checks if a num is a prime no.
+* @n: param
+*/
+
+int is_prime_number(int n)
+{
+if (n < 2)
+{
+return (0);
+}
+
+return (prime(2, n));
+}
