7c5734e284f0431a9e2360bc263001d2018cb421
Geekelo <efuelight12@gmail.com>
2023-03-10 09:51:35 +0000

    Task 9

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - function to print arguments
+* @argc: param
+* @argv: param
+* Return: Always 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%s", argv[argc - 1]);
+return (0);
+}
