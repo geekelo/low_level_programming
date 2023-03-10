940926398cf0ccb06eb31bdc1fcb29a23d5ec076
Geekelo <efuelight12@gmail.com>
2023-03-10 21:22:53 +0000

    Task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,40 +10,39 @@
 
 int main(int argc, char *argv[])
 {
-int i;
+int cents;
+int i = 0;
+int j = 0;
 int arr[5] = {25,10,5,2,1};
-int cents = atoi(argv[1]);
 
-if (argc != 3)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-while (cents >= 10)
+cents = atoi(argv[1]);
+if (cents < 0)
 {
-for (i = 0; i < 5; i++)
-	{
-		if (cents >= arr[i])
-		{
-			cents = cents - arr[i];
-		}
-	if (cents < 10)
-	{
-	break;
-	}
-	}
-	
+printf("0\n");
+return (0);
 }
-	if (cents == 0)
-	{
-		cents += 1;
-	}
-	printf("%d\n", cents);
-		return (0);
+
+
+
+	while (cents > 0)
+		{
+			if (cents >= arr[j])
+			{
+				cents = cents - arr[j];
+				i++;
+			}
+			else
+			{
+			j++;
+			}
+			
+			
+		}
+printf("%d\n", i);
+return (0);
 }	
