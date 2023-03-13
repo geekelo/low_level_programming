5b7ff835950dd0f192dfb0ccc727d6151cc62c14
Geekelo <efuelight12@gmail.com>
2023-03-13 23:09:27 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,9 @@ int k = 0;
 int m = 0;
 char *ptr = NULL;
 	if (s1 == NULL)
-	{
 		s1 = "\0";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "\0";
-	}
 	while (*(s1 + i) != '\0')
 	{
 		i++;
@@ -34,22 +30,17 @@ char *ptr = NULL;
 		i++;
 		j++;
 	}
-
 	ptr = (char *) malloc(1 + i * sizeof(char));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	for (k = 0; k < l; k++)
-		{
-			*(ptr + k) = *(s1 + k);
-		}
+		*(ptr + k) = *(s1 + k);
 	while (k < i)
-		{
-			*(ptr + k) = *(s2 + m);
-			m++;
-			k++;
-		}
+	{
+		*(ptr + k) = *(s2 + m);
+		m++;
+		k++;
+	}
 	*(ptr + i) = '\0';
 return (ptr);
 }
