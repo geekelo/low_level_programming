754e4964da853d66f66b061c02cd9c51d4d795f1
geekelo <efuelight12@gmail.com>
2023-03-13 03:42:46 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,21 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	if (size == 0)
+	{
+		return (0);
+	}
 	char *ptr = (char*) malloc(size * sizeof(c));
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c; 
 	}
-
+	
 	return (ptr);
 
 
