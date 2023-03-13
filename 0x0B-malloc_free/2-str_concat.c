d478e0c97bcc2294b65cebe80377e91d34d6e4c6
Geekelo <efuelight12@gmail.com>
2023-03-13 22:22:11 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,52 +8,42 @@
 
 char *str_concat(char *s1, char *s2)
 {
-
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int l = 0;
-	int m = 0;
-
+int i = 0; int j = 0; int l = 0; int k = 0; int m = 0;
+char *ptr = NULL;
 	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		s2 = '\0';
 	}
-
-	while (*s1[i] != '\0')
+	while (*(s1 + i) != '\0')
 	{
-		i++;
-		l++;
+	i++;
+	l++;
 	}
-
-	while (*s2[j] != '\0')
-	{
-		i++;
-		j++;
-	}
-	
-	char *ptr = (char *) malloc(1 + i * sizeof(char))
-
-		if (ptr ==  NULL)
-			return (NULL);
-	
-		while (k < l)
-			*(ptr + k) = *s1[k];
-			k++;
-
-		while (k < i)
+	while (*(s2 + j) != '\0')
 		{
-			*(ptr + k) = *s2[m];
+			i++;
+			j++;
+		}
+
+	ptr = (char *) malloc(1 + i * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < l; k++)
+		{
+			*(ptr + k) = *(s1 + k);
+		}
+	while(k < i)
+		{
+			*(ptr + k) = *(s2 + m);
 			m++;
 			k++;
 		}
-
-		*(ptr + i) = '\0';
-
-		return (ptr);
-	}
+	*(ptr + i) = '\0';
+return (ptr);
 }
