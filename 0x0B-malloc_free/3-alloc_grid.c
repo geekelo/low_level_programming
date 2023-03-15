9f2098c5c96f0f1299fba05bfd5db88c62bf5045
Geekelo <efuelight12@gmail.com>
2023-03-15 16:52:35 +0000

    jj

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,13 +26,12 @@ for (i = 0; i < height; i++)
 	arr[i] = malloc(width * sizeof(int));
 	if (arr[i] == NULL)
 	{
-	for (i--; i >=0; i--)
+	for (i--; i >= 0; i--)
 	{
-		
 		free(arr[i]);
-		free(arr);
-		return (NULL);
-		}
+	}
+	free(arr);
+	return (NULL);
 	}
 }
 for (j = 0; j < height; j++)
