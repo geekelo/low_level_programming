5b6f1041b8da7513fd0eacc68c947ffea18df7eb
Geekelo <efuelight12@gmail.com>
2023-03-17 16:01:21 +0000

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
  * malloc_checked - allocates memory
@@ -14,7 +12,7 @@ void *malloc_checked(unsigned int b)
 {
 
 	void *arr = malloc(b);
-	
+
 	if (arr == NULL)
 		exit(98);
 
