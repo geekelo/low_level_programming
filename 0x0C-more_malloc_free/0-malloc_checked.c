72d71367c42f8f6f7ee75506a84a4ea6c33cabbd
Geekelo <efuelight12@gmail.com>
2023-03-17 15:57:58 +0000

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: param
+ * Return: memory address
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	
+	void *arr = malloc(b);
+	
+	if (arr == NULL)
+		exit(98);
+
+	return (arr);
+}
