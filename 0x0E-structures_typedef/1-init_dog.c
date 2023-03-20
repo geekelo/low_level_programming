4e0275c91a493a50c90026da0990baf8b95018ef
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-20 22:54:17 +0100

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,6 +5,7 @@
  * @name: a member
  * @age: member 2
  * @owner: member 3
+ * @d: a pointer to the new name
  *
  * Description: a function tat initialize the struct dog structur
  */
@@ -15,6 +16,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	
+
 }
 
