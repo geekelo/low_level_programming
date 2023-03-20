c5b2b9853519e5cc5fdb0329868403ce8af93bd8
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-20 22:51:40 +0100

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
 #include "dog.h"
 
 /**
+ * init_dog - initializes the members of the struct
+ * @name: a member
+ * @age: member 2
+ * @owner: member 3
  *
- *
+ * Description: a function tat initialize the struct dog structur
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	struct dog *d
-	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
 	
 }
 
