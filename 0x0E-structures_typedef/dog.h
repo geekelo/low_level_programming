f3381d15e01bb7330906787e5cac889b11732937
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-20 21:29:25 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOH_H_
+#define DOG_H_
+
+/**
+ * struct dog - a newly defined structure
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
