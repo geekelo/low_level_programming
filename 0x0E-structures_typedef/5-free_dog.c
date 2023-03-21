9633df73e3c0846d5d3134526c1f7e221e758f41
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-21 14:55:39 +0100

    Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,11 +3,12 @@
 
 /**
  * free_dog - frees the struct dog
+ * @d: pointer to an initialized struct
  */
 
 void free_dog(dog_t *d)
 {
-	if(d)
+	if(d != NULL)
 	{
 	free(d->name);
 	free(d->owner);
