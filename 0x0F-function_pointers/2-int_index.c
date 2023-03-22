b5773773309ebe430d675cd77211e80607e1db75
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-22 15:03:31 +0100

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 	for (index = 0; index < size; index++)
 	{
-		if ((cmp(array[index])) >= 0)
+		if ((cmp(array[index])) != 0) 
 			return (index);
 	}
 	}
