615a5db545edee765f62bdf8e7cd9fa84ed05d56
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-22 14:04:16 +0100

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function given as a parameter on 
+ * array_iterator - executes a function given as a parameter on
  * each element of an array.
  * @array: array of integers to print
  * @size: size of the array (length)
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t index;
 
-	if((array != NULL) && ((*action) != NULL))
+	if ((array != NULL) && ((*action) != NULL))
 		for (index = 0; index < size; index++)
 			action(array[index]);
 
