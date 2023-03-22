df5a08487ce233f24499c539e7aa1fd8a2ca41ed
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-22 13:18:27 +0100

    ff

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int index;
+
+	for (index = 0; index < size; index++)
+	{
+		action(array[index]);
+
+	}
+}
+
