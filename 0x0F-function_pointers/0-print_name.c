3871ae4510165d9481d3dd1db38301c8669c1b2c
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-22 12:38:47 +0100

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - calls a function and passes a name to
@@ -11,6 +12,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if ((name != NULL) && ((*f) != NULL))
 		f(name);
 }
+
