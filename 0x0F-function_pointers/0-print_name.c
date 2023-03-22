a9f20cb66ddaca8d78c17d93d80747e5a6ee52be
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-22 12:21:53 +0100

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
+ * print_name - calls a function and passes a name to
+ * be printed inside a function pointer
+ * @name: the name to be printed 
+ * @(*f)(char *): function pointer to a function meant
+ * to print the name
  */
 
 
