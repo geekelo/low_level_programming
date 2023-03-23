076048995f2a09de8f778258a6777ba257e0e191
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-23 14:21:28 +0100

    Task 1

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	print_numbers(NULL, 4, 0, 98, -1024, 402);
+	print_numbers(NULL, 0, 0, 98, -1024, 402);
 	return (0);
 }
 
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,6 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (n == 0)
+		printf("\n");
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
