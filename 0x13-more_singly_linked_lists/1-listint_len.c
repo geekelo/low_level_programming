459c23201d69b9f06dfe46bad71598edeb7296a4
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-01 23:51:47 +0100

    a function that counts the number of nodes on a listint_t type list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
+ * listint_len - counts all the nodes in a link list
+ * @h: the first node on the link list
+ * Return: the total number of nodes on the link list
  */
 
 size_t listint_len(const listint_t *h)
@@ -10,6 +11,7 @@ size_t listint_len(const listint_t *h)
 	size_t count = 0;
 
 	listint_t *temp;
+
 	if (h)
 	{
 	temp = h->next;
