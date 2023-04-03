26be86d827891666e61e2af387b0e6b587aefc84
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-03 20:55:57 +0100

    bug fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,5 +16,6 @@ void free_listint2(listint_t **head)
 		*head = temp->next;
 		free(temp);
 	}
+	*head = NULL;
 }
 
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,11 +12,13 @@ int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
 	int n;
-
+	if (*head)
+	{
 	tmp = *head;
 	*head = (*head)->next;
 	n = tmp->n;
 	free(tmp);
+	}
 	return (n);
 }
 
