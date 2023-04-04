f2b8018318864e1567330929ba4bf2d3fc230d12
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-05 00:09:04 +0100

    Bug fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,6 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (!head)
-		return;
 	if (*head)
 	{
 		temp = *head;
@@ -27,8 +25,6 @@ void free_listint2(listint_t **head)
 
 void free_listint(listint_t *head)
 {
-	if (!head)
-		return;
 	listint_t *temp;
 
 
