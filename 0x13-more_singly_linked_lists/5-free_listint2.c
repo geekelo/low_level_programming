c3485f27270cbc0cbfb0a9aa7ee24ae96dfc3355
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-04 23:11:35 +0100

    Bug fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,20 +8,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp, *set;
 
-	if (*head)
-	{
-	while (*head)
-	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
-	}
+	temp = *head;
+
 	*head = NULL;
-
+	while (temp)
+	{
+		set = temp;
+		temp = temp->next;
+		free(set);
 	}
-
-	return;
+	free(temp);
 }
 
