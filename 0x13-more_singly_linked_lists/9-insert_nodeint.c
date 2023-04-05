d317c033ff6c4beb3620b313e3b598ccbb5cec37
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-05 17:19:28 +0100

    bug fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			count++;
 			temp = temp->next;	
 		}
-		if (count < idx)
+		if ((count + 1) < idx)
 		{
 			return (NULL);
 		}
