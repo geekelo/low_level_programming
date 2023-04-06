76c15d21a6e7ddf73e29d7a13b9bd8fa2d3e2f8b
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-06 13:09:38 +0100

    bug fixes

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at a given position
+ * @head: pointer to the first node on the list
+ * @index: given position
+ * Return: 1 on success and -1 if unsuccessful
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	if ((*head) && (index))
+	{
+		listint_t *tmp, *set;
+		unsigned int count = 1;
+
+		tmp = *head;
+
+		if (index == 0)
+		{
+			set = tmp;
+			tmp = tmp->next;
+
+
+		}
+		while (count < index)
+		{
+			tmp = tmp->next;
+			count++;
+		}
+		set = tmp->next;
+		tmp->next = set->next;
+		free(set);
+		return (1);
+	}
+	return (-1);
+}
+
