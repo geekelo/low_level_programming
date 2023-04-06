cb3e15764b14fbb6671095305ed4965ca0bbf872
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-06 08:31:07 +0100

    bug fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		newnode->n = n;
 		temp = *head;
-		if (idx == 0)
+		if ((idx == 0) && (head == NULL))
 		{
 			temp->next = newnode;
 			*head = newnode;
