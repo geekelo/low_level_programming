4db515ab919b25b39f6214d770ab1d1babb5cdbf
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-12 21:40:49 +0100

    function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: the decimal number passed in
+ * @index: the position on n base 2 format to change to 1
+ * Return: -1 if not possible and 1 if possible
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+
+	if (!((*n >> index) & 1))
+		*n = *n + (1 << index);
+	return (1);
 }
 
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@ unsigned int _pow(unsigned int base, int power);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
 
