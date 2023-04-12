49077533dfc7615bf0d89691ed51db7fc5290286
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-04-13 00:17:18 +0100

    sets the value of a bit to 0 at a given index.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear - sets the value of a bit to 0 at a given index.
+ * @n: the base 10 input
+ * @index: the positionto turn off
+ * Return: 1 if successful and -1 if not possible
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(*n) * 8)
+		return (-1);
+
+	/* ~ turns all values on except the target possition */
+	*n = *n & ~(1 << index);
+	return (1);
+}
+
